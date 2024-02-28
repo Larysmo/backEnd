71132b5d3ee76f253c46396a7c0ea2b3b93a90c4
laryssa <laryss.moraes@#gmail.com>
2024-02-27 21:56:44 -0300

    inclui modelo e controlador

--- aulas/aula02/index.js
+++ aulas/aula02/index.js
@@ -1,52 +1,6 @@
 const readline = require('readline-sync');
 
-const produtos = [];
-
-function listar(){
-    produtos.forEach(produto =>
-        console.log('Produto: ', produto.nome, '- Preço: ',produto.preco));
-}
-
-function criar(){
-    const nome = readline.question('Entre com o nome do produto: ');
-    const preco = readline.question('Entre com o preco do produto: ');
-    const novo = {nome, preco};
-    produtos.push(novo); 
-}
-
-function buscar(){
-    const nome = readline.question('Entre com o nome do produto:');
-    const buscou = produtos.find(produto => produto.nome === nome);
-    if (buscou){
-        console.log('Produto: ', buscou.nome, '- Preço: ',buscou.preco);
-    }else{
-        console.log('Produto não encontado. ):')
-    }
-}
-
-function atualizar(){
-    const nome = readline.question('Entre com o nome do produto:');
-    const buscou = produtos.find(produto => produto.nome === nome);
-    if (buscou){
-        const preco = readline.question('Entre com o novo preco: ');
-        buscou.preco = preco;
-    }else{
-        console.log('Produto não encontado. ):')
-    }
-}
-
-function remover(){
-    const nome = readline.question('Entre com o nome do produto: ');
-    const posicao = 
-        produtos.findIndex(produto => produto.nome === nome);
-    if (posicao >= 0 ){
-        produtos.splice(posicao, 1);
-    }else{
-        console.log('Produto não encontrado.')
-    }
-}
-
-
+const controlador = require('./controlador')
 
 function menu(){
     console.log('1.Listar');
@@ -59,11 +13,11 @@ function menu(){
 
 function escolherOpcao(opcao){
     switch(opcao){
-        case'1': listar(); break;
-        case'2': criar(); break;
-        case'3': buscar(); break;
-        case'4': atualizar(); break;
-        case'5': remover(); break;
+        case'1': controlador.listar(); break;
+        case'2': controlador.criar(); break;
+        case'3': controlador.buscar(); break;
+        case'4': controlador.atualizar(); break;
+        case'5': controlador.remover(); break;
         case'6': process.exit(0);
         default: console.log('Opção inválida')
     }
